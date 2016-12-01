95e80d210886122b058bbd87a4abfd19d87b724e
Olav Cleemann <olav@adapt.dk>
2016-12-01 14:00:22 +0100

    Add check for Linux::Inotify2 perl module

--- install.sh
+++ install.sh
@@ -6,10 +6,15 @@ echo This install script is quite naive. Feel free to send ideas for improvement
 INPATH=`echo $PATH | grep /usr/local/bin`
 HASPRG=`which iwatch`
 HASEVNT=`perl -e 'use Event' 2>&1`
+HASINO=`perl -e 'use Linux::Inotify2' 2>&1`
 
 if [ -n "$HASEVNT" ]; then 
 	echo 'Requires the perl-module Event'
 	echo 'You can install it by running eq: [sudo] cpan install Event'
+fi
+if [ -n "$HASINO" ]; then 
+	echo 'Requires the perl-module Linux::Inotify2'
+	echo 'You can install it by running eq: [sudo] cpan install Linux::Inotify2'
 fi
 if [ -z "$INPATH" ]; then echo 'Requires /usr/local/bin to be in $PATH'; exit 1; fi
 if [ -n "$HASPRG" ]; then echo 'You already have an iwatch program!'; echo "- in: $HASPRG"; exit 1; fi
